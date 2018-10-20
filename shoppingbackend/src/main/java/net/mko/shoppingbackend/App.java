6a501be548c02e72294b15d028fea7ffbd695ea1
Marko_I <mi016506328@fer.hr>
2018-10-20 23:18:55 +0200

    My project setup completed successfully

---
+++ shoppingbackend/src/main/java/net/mko/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.mko.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
